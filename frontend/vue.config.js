8bdb9402891253d468c545cc3c81f24a4edea4de
Jonas Delrue <jonas.delrue@jimber.org>
2020-03-31 10:22:33 +0200

    adding devserver

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
     transpileDependencies: ['vuetify'],
+    devServer: {
+        proxy: {
+          '/api': {
+            logLevel: 'debug',
+            target: 'http://localhost:5000'
+          }
+        }
+      }
 };
