8f7a86fb2aea4e76df0eb74bef19ddf8a6232f20
d <d>
2020-03-31 18:55:02 +0200

    merge conflict

--- frontend/src/views/Status.vue
+++ frontend/src/views/Status.vue
@@ -1,12 +1,18 @@
 <template>
     <div class="status">
-<<<<<<< HEAD
         <v-container class="fill-height" fluid>
             <v-row align="center" justify="center">
                 <v-card class="mt-5 py-6 mx-auto" width="800" tile>
                     <v-container fluid>
                         <v-row>
                             <v-col cols="12" sm="12">
+                                <v-btn
+                                    :to="{
+                                        name: 'signup_reffered',
+                                        params: { userid: userId },
+                                    }"
+                                    >test</v-btn
+                                >
                                 <p>
                                     Copy following url to invite friends to the
                                     3bot app
@@ -17,12 +23,13 @@
                                     label="linkske"
                                     single-line
                                     append-icon="fas fa-copy"
-                                ></v-text-field>
+                                    >{{ refererToken }}</v-text-field
+                                >
                             </v-col>
                         </v-row>
                         <v-row>
                             <v-col cols="12" sm="12">
-                                <h2>referred users ({{referrals.length}})</h2> 
+                                <h2>referred users ({{ referrals.length }})</h2>
                             </v-col>
                             <v-col cols="12" sm="12">
                                 <v-data-table
@@ -37,14 +44,6 @@
                 </v-card>
             </v-row>
         </v-container>
-=======
-        <h1>status</h1>
-        invitelink: {{refererToken}}
-        <v-btn :to="{name: 'signup_reffered', params:{'userid': userId}}">test</v-btn>
-        <code>
-            {{ referrals }}
-        </code>
->>>>>>> 1d891d4a6e9f9a8fda27e92a0f1731b000b51a70
     </div>
 </template>
 
@@ -73,23 +72,19 @@
             ).data.data;
         },
         data: function() {
-<<<<<<< HEAD
             return {
                 verifyToken: 'sdfsdfsdf',
                 referrals: null,
+                refererToken: null,
+                userId: null,
                 headers: [
                     {
                         text: '3Bot Name',
                         align: 'start',
                         value: '3BotName',
                     },
-                ],
-            };
-=======
-            return { verifyToken: null, referrals: null, refererToken: null, userId: null };
->>>>>>> 1d891d4a6e9f9a8fda27e92a0f1731b000b51a70
-        },
-    };
+                ]
+            }
+        }
+    }
 </script>
-
-<style scoped></style>
