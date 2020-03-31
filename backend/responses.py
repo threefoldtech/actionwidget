c7978edafc91ba2a289c7f6b4d777ac5f5092744
Jonas Wijne <jonas.wijne@jimber.org>
2020-03-31 14:09:28 +0200

    fix typo

--- backend/responses.py
+++ backend/responses.py
@@ -2,7 +2,7 @@ import json
 from db.object_encoder import ObjectEncoder
 
 def successObj(code, data):
-    return json.dumps({ 'succes': True, 'data': data }, cls=ObjectEncoder), code
+    return json.dumps({ 'success': True, 'data': data }, cls=ObjectEncoder), code
 
 def errorObj(code, message):
     return json.dumps({ 'error': { 'code': code, 'message': message} },  cls=ObjectEncoder), code
