e7a03b45500482cb73045e9cc1143ebca13dc60d
Jonas Wijne <jonas.wijne@jimber.org>
2020-04-01 19:00:15 +0200

    add prod conf

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -3,7 +3,7 @@ export default {
     appId: window.location.host,
     seedPhrase:
         'weather smooth little world side palace green armor busy view solution escape',
-    botFrontEnd: 'https://login.staging.jimber.org',
-    botBackend: 'https://login.staging.jimber.org',
-    kycBackend: 'https://openkyc.staging.jimber.org',
+    botFrontEnd: 'https://login.threefold.me',
+    botBackend: 'https://login.threefold.me/',
+    kycBackend: 'https://openkyc.live',
 };
